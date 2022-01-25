0d96893e703e0ee0f374ce3d683b1040fcac2b73
Jonathan <41275844+Jonathhhan@users.noreply.github.com>
2022-01-25 22:22:09 +0100

    Update library_pdMethods.js

--- extra/library_pdMethods.js
+++ extra/library_pdMethods.js
@@ -1,27 +1,26 @@
 function loadAudio() { 
+   var offlineCtx = new OfflineAudioContext(2,44100,44100);
+   var input = document.createElement('input');
+   input.type = 'file';
+   input.onchange = e => { 
 
-var offlineCtx = new OfflineAudioContext(2,44100,44100);
-var input = document.createElement('input');
-input.type = 'file';
+      // getting a hold of the file reference
+      var file = e.target.files[0]; 
 
-input.onchange = e => { 
+      // setting up the reader
+      var reader = new FileReader();
+      reader.readAsArrayBuffer(file);
 
-   // getting a hold of the file reference
-   var file = e.target.files[0]; 
+      // here we tell the reader what to do when it's done reading...
+      reader.onload = readerEvent => {
+         var arrayBuffer = reader.result;
+         offlineCtx.decodeAudioData(arrayBuffer, decodedDone);
+      }
 
-   // setting up the reader
-   var reader = new FileReader();
-   reader.readAsArrayBuffer(file);
-
-   // here we tell the reader what to do when it's done reading...
-   reader.onload = readerEvent => {
-    var arrayBuffer = reader.result;
-    offlineCtx.decodeAudioData(arrayBuffer, decodedDone);
-    }
-function decodedDone(decoded) {
- Module.audioInLeft(decoded.getChannelData(0));
- Module.audioInRight(decoded.getChannelData(1));
-  }
-}
-input.click();
-  }
+   function decodedDone(decoded) {
+      Module.audioInLeft(decoded.getChannelData(0));
+      Module.audioInRight(decoded.getChannelData(1));
+      }
+   }
+   input.click();
+   }
